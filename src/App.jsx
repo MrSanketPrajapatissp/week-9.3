060a25fabb58d821827ac25964172be595ce97c0
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2025-03-24 11:27:40 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,28 @@
-import React, { use, useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import useSWR from "swr";
 
-
-function useTodos(n) {
-  const [todos, setTodos] = useState([]); // Ensure initial state is an array
-  const [loading, setLoading] = useState(true);
-
-  function getData()
-  {
-    axios.get(`https://dummyjson.com/todos`).then((res) => {
-      // The API returns a single object, so wrap it in an array
-      setTodos(res.data.todos);
-      setLoading(false);
-    });
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch");
   }
-
-  useEffect(() => {
-     setInterval(() => {
-      getData();
-     },n * 1000)
-     getData();
-  }, [n]);
-
-  return { todos: todos,
-     loading:loading };
-}
+  return response.json();
+};
 
 function App() {
-  const { todos, loading } = useTodos(5);
+  const { data, error, isLoading } = useSWR(`https://dummyjson.com/todos`, fetcher);
 
-  if (loading) {
-    return <div> loading ...</div>;
+  if (error) {
+    return <div>Failed to Load</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
   }
 
   return (
     <div>
-      {todos.map((todo) => (
+      {data.todos.map((todo) => (
         <Track key={todo.id} todo={todo} />
       ))}
     </div>

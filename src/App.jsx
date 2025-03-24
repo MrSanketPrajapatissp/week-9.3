8fa6f0601494afa169db5e06424e1905367da131
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2025-03-24 10:17:15 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,53 @@
-import { use } from "react";
-import "./App.css";
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { use, useEffect, useState } from "react";
+import axios from "axios";
+
+
+function useTodos(n) {
+  const [todos, setTodos] = useState([]); // Ensure initial state is an array
+  const [loading, setLoading] = useState(true);
+
+  function getData()
+  {
+    axios.get(`https://dummyjson.com/todos`).then((res) => {
+      // The API returns a single object, so wrap it in an array
+      setTodos(res.data.todos);
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+     setInterval(() => {
+      getData();
+     },n * 1000)
+     getData();
+  }, [n]);
+
+  return { todos: todos,
+     loading:loading };
+}
 
 function App() {
+  const { todos, loading } = useTodos(5);
 
-  const [render, setRender] = useState("true");
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setRender(false);
-  //   }, 10000)
-  // }, [])
-  
-  useEffect(() => {
-    setInterval(() => {
-     setRender(r => !r)
-    },5000)
-  },[])
+  if (loading) {
+    return <div> loading ...</div>;
+  }
 
   return (
-    <>
-      { render ? <MyComponent/> : <div></div>}
-    </>
+    <div>
+      {todos.map((todo) => (
+        <Track key={todo.id} todo={todo} />
+      ))}
+    </div>
   );
 }
 
-function MyComponent() {
-  
-  useEffect(() => {
-    console.error("Component Mounted!");
-
-    return () => {
-      console.log("Component UnMounted!");
-    };
-  }, []);
-
-  return <div>Form inside MyComponent.</div>;
+function Track({ todo }) {
+  return (
+    <div>
+      <div>{todo.todo}</div>
+    </div>
+  );
 }
 
 export default App;
